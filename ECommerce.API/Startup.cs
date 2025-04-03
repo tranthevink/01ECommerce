2272e42fd2ae8ccbed47cfe09b96da8d0c52d9ae
tranthevink <tranthevink@gmail.com>
2025-04-03 19:08:11 +0700

    update project architecture

--- ECommerce.API/Startup.cs
+++ ECommerce.API/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 
-
 namespace ECommerce.API
 {
     public class Startup
@@ -25,15 +24,31 @@ namespace ECommerce.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllersWithViews();
 
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
 
             services.AddSingleton<ILoggerService, LoggerService>();
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer("Server=.;Database=MyApp;Trusted_Connection=True;"));
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                options.UseLazyLoadingProxies()
+                       .UseSqlServer("Server=.;Database=MyApp;Trusted_Connection=True;"));
+            services.AddAuthentication(options =>
+                    {
+                        options.DefaultScheme = "MyCookie";
+                        options.DefaultAuthenticateScheme = "MyCookie";
+                        options.DefaultChallengeScheme = "MyCookie";
+                    })
+                    .AddCookie("MyCookie", options =>
+                    {
+                        options.Cookie.Name = "MyCookie";
+                        options.Cookie.SameSite = SameSiteMode.Strict;
+                        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                        options.Cookie.HttpOnly = true;
+                        options.Cookie.IsEssential = true;
+                        options.LoginPath = "/Account/Login";
+                        options.LogoutPath = "/Account/Logout";
+                    })
                     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
                     {
                         options.Authority = "https://localhost:5001";
@@ -45,7 +60,7 @@ namespace ECommerce.API
         // Cấu hình middleware pipeline
         public void Configure(IApplicationBuilder app)
         {
-            
+
             if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
